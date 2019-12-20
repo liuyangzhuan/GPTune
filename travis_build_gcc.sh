8f0494c6da2cb8e855068ac7be27088bbd950fe2
liuyangzhuan <liuyangzhuan@gmail.com>
2019-12-19 19:43:24 -0800

    retrying

--- travis_build_gcc.sh
+++ travis_build_gcc.sh
@@ -2,6 +2,6 @@
 
 touch build_gcc.log
 sh log_monitor.sh build_gcc.log &
-#../configure -v --build=x86_64-linux-gnu --host=x86_64-linux-gnu --target=x86_64-linux-gnu --prefix=$PWD/../ --enable-checking=release --enable-languages=c,c++,fortran --disable-multilib &>build_gcc.log
-make -j8 &>build_gcc.log
+../configure -v --build=x86_64-linux-gnu --host=x86_64-linux-gnu --target=x86_64-linux-gnu --prefix=$PWD/../ --enable-checking=release --enable-languages=c,c++,fortran --disable-multilib &>>build_gcc.log
+make -j8 &>>build_gcc.log
 echo "this is finished" >> build_gcc.log
